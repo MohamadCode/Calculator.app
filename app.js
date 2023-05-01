01128bac9903eb3f02c1a7247945c152c1c266da
MohamadCode <m-munla2004@hotmail.com>
2023-05-01 20:13:09 +0300

    1st-fucntion__start

--- app.js
+++ app.js
@@ -11,5 +11,16 @@ document.getElementById("num2-el").textContent = num2
 // Perform the given calculation using num1 and num2
 // Render the result of the calculation in the paragraph with id="sum-el"
 
+function add() {
+    
+}
+
+
+
+
+
+
+
+
 // E.g. if the user clicks on the "Plus" button, you should render
 // "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
